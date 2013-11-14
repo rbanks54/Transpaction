f7d0ce10f55f521d572344bfb81d7657b59b91cc
Richard Banks <rbanks54@msn.com>
2013-11-14 13:31:48 +1100

    basic collection binding in KO

---
+++ TranSPAaction/Scripts/transactions.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+function transactionModel(date, details, credit, debit) {
+    var self = this;
+    self.date = ko.observable(date);
+    self.details = ko.observable(details);
+    self.creditAmount = ko.observable(credit || 0);
+    self.debitAmount = ko.observable(debit || 0);
+};
+
+function transactionsViewModel() {
+    var self = this;
+    self.transactions = ko.observableArray([
+        new transactionModel('1/1/2013', 'first', 10),
+        new transactionModel('2/1/2013', 'second',undefined,10)
+    ]);
+};
+
+ko.applyBindings(new transactionsViewModel());
