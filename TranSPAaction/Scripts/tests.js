0404778de2af844cea82bfef245c9599212be45d
Richard Banks <rbanks54@msn.com>
2013-11-14 15:51:09 +1100

    extra test, and wrapped into a self executing function

--- TranSPAaction/Scripts/tests.js
+++ TranSPAaction/Scripts/tests.js
@@ -2,7 +2,26 @@
 /// <reference path="qunit-1.12.0.js" />
 /// <reference path="transactions.js" />
 
-test('transaction viewmodel', function () {
-    var viewModel = new transpaction.transactionsViewModel();
-    QUnit.ok(viewModel.transactions().length === 2,'');
-});
+(function() {
+
+    var viewModel = {};
+
+    QUnit.module('transactions', {
+        setup : function() {
+            viewModel = new transpaction.transactionsViewModel(); 
+        }
+    });
+
+    QUnit.test('transaction viewmodel', function () {
+        QUnit.ok(viewModel.transactions().length === 2,'');
+    });
+
+    QUnit.test('should all have dates', function(assert) {
+        var transactions = viewModel.transactions();
+        for (var i = 0; i < transactions.length; i++) {
+            assert.ok(typeof(transactions[i].date() !== 'undefined'));
+            assert.ok(transactions[i].date() !== null);
+        }
+    });
+
+})();
