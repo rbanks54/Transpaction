03678e85468bb7993646e839cfa5e4a015041ba2
Richard Banks <rbanks54@msn.com>
2013-11-26 13:38:06 +1100

    oops

---
+++ TestProject/TransactionsSteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using Transpaction;
+using System.Linq;
+
+namespace TestProject
+{
+    [Binding]
+    public class TransactionsSteps
+    {
+        private TransactionService service;
+
+        [Given(@"I have an account with (.*) transactions")]
+        public void GivenIHaveAnAccountWithTransactions(int p0)
+        {
+            service = new TransactionService();
+        }
+
+        [When(@"I ask for all transactions")]
+        public void WhenIAskForAllTransactions()
+        {
+            var result = service.Get(new TransactionRequest());
+            ScenarioContext.Current.Add("result",result);
+        }
+        
+        [Then(@"(.*) transactions should be returned")]
+        public void ThenTransactionsShouldBeReturned(int p0)
+        {
+            var result = ScenarioContext.Current["result"]
+                as IList<Transaction>;
+            Assert.AreEqual(p0,result.Count());
+        }
+    }
+}
---
+++ TestProject/transactions.feature
@@ -0,0 +1,9 @@
+﻿Feature: transactions
+	As an account holder
+	I want to see all my transactions
+	So I can balance my books
+
+Scenario: Shows all transactions
+	Given I have an account with 10 transactions
+	When I ask for all transactions
+	Then 10 transactions should be returned
